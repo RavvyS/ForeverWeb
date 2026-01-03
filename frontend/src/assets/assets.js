555af2744c8b80312dc1e1a158d8e4c35950d75b
RavvyS <156438913+RavvyS@users.noreply.github.com>
2026-01-03 22:41:17 +0530

    resposive Narbar

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,10 +1,18 @@
 // Import images
 import logo from ".//logo.png";
 import search_icon from ".//search.png";
+import profile_icon from ".//profile.png";
+import cart_icon from ".//cart.png";
+import menu_icon from ".//menu.png";
+import dropdown_icon from ".//arrow.png";
 
 // Export as an object
 export const assets = {
   logo,
-  search_icon
+  search_icon,
+  profile_icon,
+  cart_icon,
+  menu_icon,
+  dropdown_icon
 
 };
